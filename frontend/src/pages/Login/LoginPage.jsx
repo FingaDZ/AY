962a7fba5f9b9baadd4b31487ba064bfe61f9be5
FingaDZ <finga@ayhr.dz>
2025-12-05 15:05:08 +0100

    v2.4.2 - Update all version displays (Login, Sidebar) to 2.4.2

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -87,7 +87,7 @@ function LoginPage() {
         </form>
 
         <div className="mt-6 text-center">
-          <div className="text-xs text-gray-400">Version 2.0.1</div>
+          <div className="text-xs text-gray-400">Version 2.4.2</div>
           <div className="text-xs text-gray-400 mt-1 font-medium">Powered By AIRBAND</div>
         </div>
       </div>
